dab860d2fad5672fd60f69b2b932f4fc13b9054e
Denys Vasyliev <den.vasyliev@gmail.com>
2023-02-22 00:25:13 +0000

    add more comments

--- variables.tf
+++ variables.tf
@@ -1,17 +1,21 @@
 variable "github_repository" {
-  type = string
+  type        = string
+  description = "GitHub repository to store Flux manifests"
 }
 
 variable "target_path" {
-  type    = string
-  default = "clusters"
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
 }
 
 variable "private_key" {
-  type = string
+  type        = string
+  description = "The private key used to authenticate with the Git repository"
 }
 
 variable "config_path" {
-  type    = string
-  default = "~/.kube/config"
+  type        = string
+  default     = "~/.kube/config"
+  description = "The path to the kubeconfig file"
 }
