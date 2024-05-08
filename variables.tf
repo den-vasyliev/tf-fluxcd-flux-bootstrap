3900fdb522a4b8993b7271f3ff739dd86ec34e7a
den <den@dens-laptop.home>
2024-05-08 14:44:39 +0100

    add validation

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
 variable "github_repository" {
   type        = string
   description = "GitHub repository to store Flux manifests"
+
+  validation {
+    condition     = length(var.github_repository) > 0
+    error_message = "The github_repository must not be empty."
+  }
 }
 
 variable "flux_namespace" {
@@ -19,11 +24,22 @@ variable "github_token" {
   type        = string
   default     = ""
   description = "The token used to authenticate with the Git repository"
+
+  validation {
+    condition     = length(var.github_token) > 0
+    error_message = "The github_token must not be empty."
+  }
 }
 
 variable "private_key" {
   type        = string
+  default     = ""
   description = "The private key used to authenticate with the Git repository"
+
+  validation {
+    condition     = length(var.private_key) > 0
+    error_message = "The private_key must not be empty."
+  }
 }
 
 variable "config_host" {
@@ -36,10 +52,19 @@ variable "config_token" {
   type        = string
   default     = "token"
   description = "The token for gke"
+
+  validation {
+    condition     = length(var.config_token) > 0
+    error_message = "The config_token must not be empty."
+  }
 }
 
 variable "config_ca" {
   type        = string
   default     = "ca"
   description = "The ca for gke"
-}
+  validation {
+    condition     = length(var.config_ca) > 0
+    error_message = "The config_ca must not be empty."
+  }
+ }
